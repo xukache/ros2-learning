e774bbb45ea08f295f3b345fb8384da40aee3673
xukache <2416695482@qq.com>
2023-06-05 22:56:55 +0800

    第3章通信机制补充-实操-话题通信BUG修复

---
+++ ws01_plumbing/build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/xukai/00-Learn/ros2-learning/ws01_plumbing/src/turtlesim
+# Build directory: /home/xukai/00-Learn/ros2-learning/ws01_plumbing/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
